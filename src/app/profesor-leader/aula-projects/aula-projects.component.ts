cc6e358e7795f5bc488c8d50ded0572c6b0a412f
Johan Leonardo Ibarra Sandoval <johanleonardois@ufps.edu.co>
2023-06-02 13:12:42 -0500

    Dashboard

--- src/app/profesor-leader/aula-projects/aula-projects.component.ts
+++ src/app/profesor-leader/aula-projects/aula-projects.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
+import { AulaProject } from 'src/app/interfaces/aulaProject.interface';
+import { AulaProjectsService } from 'src/app/services/aula-projects.service';
 
 @Component({
   selector: 'app-aula-projects',
   templateUrl: './aula-projects.component.html',
-  styleUrls: ['./aula-projects.component.css']
+  styleUrls: ['./aula-projects.component.css'],
 })
 export class AulaProjectsComponent {
+  private _aulaProjects: AulaProject[];
 
+  public constructor(private aulaProjectsService: AulaProjectsService) {
+    this._aulaProjects = [];
+  }
+
+  ngOnInit() {
+    this.aulaProjectsService.list().subscribe({
+      next: (aulaProjectsList) => (this._aulaProjects = aulaProjectsList),
+    });
+  }
+
+  get aulaProjects(): AulaProject[] {
+    return this._aulaProjects.filter((x, y) => y < 4)
+  }
 }
--- src/app/workplans/workplans.module.ts
+++ src/app/workplans/workplans.module.ts
@@ -4,6 +4,7 @@ import { ViewWorkPlansComponent } from './list-work-plans/list-work-plans.compon
 import { WorkPlansComponent } from './work-plans/work-plans.component';
 import { ActivitiesComponent } from './activities/activities.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 
 
@@ -13,7 +14,7 @@ import { ReactiveFormsModule } from '@angular/forms';
     WorkPlansComponent,
     ActivitiesComponent,
   ],
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [CommonModule, ReactiveFormsModule, RouterModule],
   exports: [ViewWorkPlansComponent, WorkPlansComponent, ActivitiesComponent],
 })
 export class WorkplansModule {}
